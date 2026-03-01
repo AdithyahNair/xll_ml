4eda0b3375c373d07f611ba37b2d5ecc39f33c42
Adithyah Nair <adithyah.nair@gmail.com>
2026-03-01 11:45:24 -0800

    completed todods

--- xll_curve_pwflat.cpp
+++ xll_curve_pwflat.cpp
@@ -1,17 +1,15 @@
 // xll_curve.cpp - curve functions
 #include "fms_curve_pwflat.h"
 #include "xll_fi.h"
-
 using namespace xll;
 using namespace fms;
-
 static AddIn xai_curve_pwflat_(
 	Function(XLL_HANDLEX, L"xll_curve_pwflat_", L"\\" CATEGORY L".CURVE.PWFLAT")
 	.Arguments({
 		Arg(XLL_FP, L"t", L"is the vector of forward rate times."),
 		Arg(XLL_FP, L"f", L"is the vector of forward rates."),
 		})
-	.Uncalced()
+		.Uncalced()
 	.Category(CATEGORY)
 	.FunctionHelp(L"Return a handle to a curve with forward rates f at times t.")
 );
@@ -19,7 +17,6 @@ HANDLEX WINAPI xll_curve_pwflat_(_FP12* pt, _FP12* pf)
 {
 #pragma XLLEXPORT
 	HANDLEX h = INVALID_HANDLEX;
-
 	try {
 		handle<curve::base<>> h_(new curve::pwflat(span(*pt), span(*pf)));
 		ensure(h_);
@@ -31,23 +28,20 @@ HANDLEX WINAPI xll_curve_pwflat_(_FP12* pt, _FP12* pf)
 	catch (...) {
 		XLL_ERROR(__FUNCTION__ ": unknown exception");
 	}
-
 	return h;
 }
-
 static AddIn xai_curve_pwflat(
 	Function(XLL_FP, L"xll_curve_pwflat", CATEGORY L".CURVE.PWFLAT")
 	.Arguments({
 		Arg(XLL_HANDLEX, L"h", L"is a handle to a pwflat curve."),
 		})
-	.Category(CATEGORY)
+		.Category(CATEGORY)
 	.FunctionHelp(L"Return a two row array of times and rates.")
 );
 _FP12* WINAPI xll_curve_pwflat(HANDLEX h)
 {
 #pragma XLLEXPORT
 	static FPX tf;
-
 	try {
 		tf.resize(0, 0);
 		handle<curve::base<>> h_(h);
@@ -64,12 +58,8 @@ _FP12* WINAPI xll_curve_pwflat(HANDLEX h)
 	catch (...) {
 		XLL_ERROR(__FUNCTION__ ": unknown exception");
 	}
-
 	return tf.get();
 }
-
-// TODO:NE Implement CURVE.FORWARD, CURVE.DISCOUNT, CURVE.SPOT
-// use `handle<curve::base<>> h_(h)
 AddIn xai_curve_forward(
 	Function(XLL_DOUBLE, L"?xll_curve_forward", CATEGORY L".CURVE.FORWARD")
 	.Arguments({
@@ -83,11 +73,9 @@ double WINAPI xll_curve_forward(HANDLEX c, double t)
 {
 #pragma XLLEXPORT
 	double result = math::NaN<>;
-
 	try {
 		handle<curve::base<>> c_(c);
 		ensure(c_);
-
 		result = c_->forward(t);
 	}
 	catch (const std::exception& ex) {
@@ -96,10 +84,8 @@ double WINAPI xll_curve_forward(HANDLEX c, double t)
 	catch (...) {
 		XLL_ERROR(__FUNCTION__ ": unknown exception");
 	}
-
 	return result;
 }
-
 AddIn xai_curve_discount(
 	Function(XLL_DOUBLE, L"?xll_curve_discount", CATEGORY L".CURVE.DISCOUNT")
 	.Arguments({
@@ -113,11 +99,9 @@ double WINAPI xll_curve_discount(HANDLEX c, double t)
 {
 #pragma XLLEXPORT
 	double result = math::NaN<>;
-
 	try {
 		handle<curve::base<>> c_(c);
 		ensure(c_);
-
 		result = c_->discount(t);
 	}
 	catch (const std::exception& ex) {
@@ -126,10 +110,8 @@ double WINAPI xll_curve_discount(HANDLEX c, double t)
 	catch (...) {
 		XLL_ERROR(__FUNCTION__ ": unknown exception");
 	}
-
 	return result;
 }
-
 AddIn xai_curve_spot(
 	Function(XLL_DOUBLE, L"?xll_curve_spot", CATEGORY L".CURVE.SPOT")
 	.Arguments({
@@ -143,11 +125,9 @@ double WINAPI xll_curve_spot(HANDLEX c, double t)
 {
 #pragma XLLEXPORT
 	double result = math::NaN<>;
-
 	try {
 		handle<curve::base<>> c_(c);
 		ensure(c_);
-
 		result = c_->spot(t);
 	}
 	catch (const std::exception& ex) {
@@ -156,6 +136,5 @@ double WINAPI xll_curve_spot(HANDLEX c, double t)
 	catch (...) {
 		XLL_ERROR(__FUNCTION__ ": unknown exception");
 	}
-
 	return result;
 }
